484debb890eff2e9ac6483054b62ea0cd6fed63c
priyasonar1207 <83229578+priyasonar1207@users.noreply.github.com>
2021-10-08 15:05:15 +0530

    Add project files.

---
+++ Part56OverrideToString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Part56OverrideToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine("String conversion={0}",a.ToString());
+            string s = Convert.ToString(a);
+            Console.WriteLine(s);
+            Customers c1 = new Customers
+            {
+                FirstName="Shubham",
+                LastName="Ransing"
+
+            };
+            //this is the  problem issue it prints namespace name.class name thats why i need to override this method
+            Console.WriteLine(c1.ToString());
+
+
+ 
+    }
+    }
+    class Customers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        //here we  just change the defination of to string method now method is working like our requirement
+        public override string ToString()
+        {
+            return LastName + " " + FirstName;
+        }
+
+    }
+}
